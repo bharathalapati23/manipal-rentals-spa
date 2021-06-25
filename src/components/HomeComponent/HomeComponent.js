f5e67ff4950a5f2c5b82adb4f0962800441a47c4
Bharath <bharathalapati23@gmail.com>
2021-06-25 14:39:18 +0530

    added cloudinary url to bg img

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
 
 		boxSizing: 'border-box',
 		//backgroundImage: `url(${bgImg})`,
-		backgroundImage: `url(https://res.cloudinary.com/dojfndzbj/image/upload/v1624611856/home-background_obxll3.jpg)`,
+		backgroundImage: `url(https://res.cloudinary.com/dojfndzbj/image/upload/f_auto,q_auto/v1624611856/home-background_obxll3.jpg)`,
 		backgroundSize: 'cover',
 		backgroundPosition: 'center center'
 	},
