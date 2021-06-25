2ea91cc5653790fb7250da995682f1b1f5a9552d
Bharath <bharathalapati23@gmail.com>
2021-06-26 00:10:05 +0530

    added react lazy

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import NavBar from './components/NavBar/NavBar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 //import DashboardComponent from './components/DashboardComponent'
 
-// import UploadComponent from './components/Upload/UploadComponent'
-// import PropertyInfoComponent from './components/PropertyInfoComponent/PropertyInfoComponent.js'
+import UploadComponent from './components/Upload/UploadComponent'
+//import PropertyInfoComponent from './components/PropertyInfoComponent/PropertyInfoComponent.js'
 import FooterComponent from './components/FooterComponent.js'
-// import HowItWorksComponent from './components/HowItWorks/HowItWorksComponent.js'
-// import EnquiryComponent from './components/AssistedBooking/EnquiryComponent.js'
+import HowItWorksComponent from './components/HowItWorks/HowItWorksComponent.js'
+import EnquiryComponent from './components/AssistedBooking/EnquiryComponent.js'
 import HomeComponent from './components/HomeComponent/HomeComponent'
-// import ContactUs from './components/ContactUs'
-// import AboutUsComponent from './components/AboutUsComponent'
+import ContactUs from './components/ContactUs'
+import AboutUsComponent from './components/AboutUsComponent'
 import './App.css'
 import { Helmet } from 'react-helmet';
 
 const DashboardComponent = lazy(() => import('./components/DashboardComponent'))
-const UploadComponent = lazy(() => import('./components/Upload/UploadComponent'))
-const HowItWorksComponent = lazy(() => import('./components/HowItWorks/HowItWorksComponent.js'))
-const EnquiryComponent = lazy(() => import('./components/AssistedBooking/EnquiryComponent.js'))
-const ContactUs = lazy(() => import('./components/ContactUs'))
-const AboutUsComponent = lazy(() => import('./components/AboutUsComponent'))
 const PropertyInfoComponent = lazy(() => import('./components/PropertyInfoComponent/PropertyInfoComponent.js'))
-
 
 
 function App() {
@@ -50,27 +44,19 @@ function App() {
             </Route>
             <Route path='/how-it-works' exact>
               <NavBar />
-              <Suspense fallback={<div style={{ marginTop: '200px' }}>loading...</div>}>
-                <HowItWorksComponent />
-              </Suspense>
+              <HowItWorksComponent />
             </Route>
             <Route path='/assisted-booking' exact>
               <NavBar />
-              <Suspense fallback={<div style={{ marginTop: '200px' }}>loading...</div>}>
-                <EnquiryComponent />
-              </Suspense>
+              <EnquiryComponent />
             </Route>
             <Route path='/contact-us' exact>
               <NavBar />
-              <Suspense fallback={<div style={{ marginTop: '200px' }}>loading...</div>}>
-                <ContactUs />
-              </Suspense>
+              <ContactUs />
             </Route>
             <Route path='/about-us' exact>
               <NavBar />
-              <Suspense fallback={<div style={{ marginTop: '200px' }}>loading...</div>}>
-                <AboutUsComponent />
-              </Suspense>
+              <AboutUsComponent />
             </Route>
           </>
         </Switch>
