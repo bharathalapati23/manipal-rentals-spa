af4ea0f83b990529af6fab5e4745f4f649a7796a
Vishnu Reddy <cvishnureddy5500@gmail.com>
2021-11-26 23:21:58 +0530

    added property rented-out feature

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,36 +1,35 @@
-import * as api from '../api'
-import { setSortOrder } from './sortOrder'
+import * as api from "../api";
+import { setSortOrder } from "./sortOrder";
 
 export const getPosts = (sortOrder) => async (dispatch, getState) => {
-    try {
-        if (getState().sortOrder !== sortOrder.rent) {
-            dispatch({ type: 'SET_LOADING_TRUE' })
-            dispatch(setSortOrder(sortOrder.rent))
-            const { data } = await api.fetchPosts(sortOrder);
-            dispatch({ type: 'FETCH_ALL', payload: data })
-            dispatch({ type: 'SET_LOADING_FALSE' })
-        }
-    } catch (error) {
-        console.log(error.message)
+  try {
+    if (getState().sortOrder !== sortOrder.rent) {
+      dispatch({ type: "SET_LOADING_TRUE" });
+      dispatch(setSortOrder(sortOrder.rent));
+      const { data } = await api.fetchPosts(sortOrder);
+      data.Active = 1;
+      console.log("#############################");
+      console.log(data);
+      dispatch({ type: "FETCH_ALL", payload: data });
+      dispatch({ type: "SET_LOADING_FALSE" });
     }
-
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const createPost = (post) => async (dispatch) => {
-    try {
-        const { data } = await api.createPost(post);
+  try {
+    const { data } = await api.createPost(post);
 
-        dispatch({ type: 'CREATE', payload: data })
-    } catch (error) {
-
-    }
-}
+    dispatch({ type: "CREATE", payload: data });
+  } catch (error) {}
+};
 
 export const clearPosts = () => async (dispatch) => {
-    try {
-        dispatch({ type: 'CLEAR_ALL' })
-    } catch (error) {
-        console.log(error.message)
-    }
-
-}
+  try {
+    dispatch({ type: "CLEAR_ALL" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,13 +1,13 @@
-export default (posts =[], action) => {
-    switch(action.type) {
-        case 'FETCH_ALL': 
-            return action.payload
-        case 'CREATE':
-            return [...posts, action.payload]
-        case 'CLEAR_ALL':
-            return []
-        default:
-            return posts
-    }
-
-}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return action.payload;
+    case "CREATE":
+      return [...posts, action.payload];
+    case "CLEAR_ALL":
+      return [];
+    default:
+      return posts;
+  }
+};
